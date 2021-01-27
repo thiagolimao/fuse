22d6c7f766d4819b18523e4413ae4586e6c1e89f
Thiago Limão <thiago@thiagolima.com>
2021-01-27 19:31:07 -0300

    tradução para inglês

--- dist/forms/contact.php
+++ dist/forms/contact.php
@@ -8,6 +8,7 @@
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'zap@fuse.capital';
+//   $receiving_email_address = 'storge.lima@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
