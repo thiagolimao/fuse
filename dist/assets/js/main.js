edd363223aa2241345df2baf6666db938a044821
Thiago Limão <thiago@thiagolima.com>
2022-01-24 17:30:21 -0300

    Update content

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -22,7 +22,7 @@
   });
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
-  var scrolltoOffset = $('#header').outerHeight() - 50;
+  var scrolltoOffset = $('#header').outerHeight() - 80;
   $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
